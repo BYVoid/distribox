bce388ffb6e313f8bf29b4f15a217ba6ebb63684
curimit <curimit@gmail.com>
2013-03-27 10:21:26 +0800

    Add PatchRequest.cs

---
+++ Distribox/Distribox.Network/Message/PatchRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Distribox.CommonLib;
+using Distribox.FileSystem;
+
+namespace Distribox.Network
+{
+    class PatchRequest : ProtocolMessage
+    {
+		public List<AtomicPatch> Request { get; set; }
+
+        public PatchRequest(List<AtomicPatch> request, int myPort)
+            : base(myPort)
+        {
+            Request = request;
+            _type = MessageType.FileRequest;
+		}
+		
+		public override void Accept(AntiEntropyProtocol visitor, Peer peer)
+		{
+			visitor.Process(this, peer);
+		}
+    }
+}

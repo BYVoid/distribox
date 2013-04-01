26996990197e5b2a9e165ecea37c2b6cd030eee4
Chris Chen <chris.jianfei.chen@gmail.com>
2013-04-01 21:46:11 +0800

    Add comments in Main.cs

--- Distribox/Distribox.CLI/Main.cs
+++ Distribox/Distribox.CLI/Main.cs
@@ -7,30 +7,66 @@ using Distribox.Network;
 
 namespace Distribox.CLI
 {
+    /// <summary>
+    /// Root of the CLI program.
+    /// </summary>
     class Program
     {
         static AntiEntropyProtocol protocol;
-
+  
+        /// <summary>
+        /// Used for the ruby engine.
+        /// </summary>
         public class API
         {
+            /// <summary>
+            /// Invite the specified peer at <paramref name="port"/>.
+            /// </summary>
+            /// <param name='port'>
+            /// Port of the peer to be invited.
+            /// </param>
+            /// <remarks>
+            /// Currently, we are only communicating on a single machine. So no ip is specified.s
+            /// </remarks>
             public void invite(int port)
             {
                 protocol.InvitePeer(new Peer(IPAddress.Parse("127.0.0.1"), port));
             }
         }
-        
+
+        /// <summary>
+        /// The entry point of the program, where the program control starts and ends.
+        /// </summary>
+        /// <param name='args'>
+        /// The command-line arguments.
+        /// </param>
         public static void Main(string[] args)
         {
+            // Get config
             int port = Config.GetConfig().ListenPort;
             string root = Config.GetConfig().RootFolder;
+            
+            // Initialize folder
             CommonHelper.InitializeFolder(root);
+            
+            // Start peer service
             StartPeer(port, root);
-
+   
+            // Start CLI
             RubyEngine engine = new RubyEngine();
             engine["api"] = new API();
             engine.Repl();
         }
         
+        /// <summary>
+        /// Starts the peer service.
+        /// </summary>
+        /// <param name='port'>
+        /// Listening port of the peer.
+        /// </param>
+        /// <param name='root'>
+        /// Root folder to be watched.
+        /// </param>
         private static void StartPeer(int port, string root)
         {
             string peerListName = root + Properties.PeerListFilePath;
@@ -46,7 +82,6 @@ namespace Distribox.CLI
             watcher.Deleted += vs.Deleted;
             watcher.Renamed += vs.Renamed;
             watcher.Idle += vs.Flush;
-
 
             // Create a virtual machine to repl
             RubyEngine vm = new RubyEngine();
